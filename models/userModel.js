87adc4c9bf2452e351ab80d5a3209a7ea5e28ccf
Iker 275 <jeananderson275@gmail.com>
2026-02-08 18:13:43 +1000

    refactored user model on auth

--- models/userModel.js
+++ models/userModel.js
@@ -41,16 +41,22 @@ const userSchema = new mongoose.Schema({
 { timestamps: true }
 )
 
-userSchema.pre('save', async function (next) {
+// userSchema.pre('save', async function (next) {
     
 
-    const salt = await bcrypt.genSalt();
+//     const salt = await bcrypt.genSalt();
+//     this.password = await bcrypt.hash(this.password, salt);
+//    // console.log('user about to be created',this);
+//     next;
+// })
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next;
+
+    const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-   // console.log('user about to be created',this);
     next;
-})
-
-
+});
 
 // userSchema.statics.login = async function(email, password) {
 //     const user = await this.findOne({ email });
